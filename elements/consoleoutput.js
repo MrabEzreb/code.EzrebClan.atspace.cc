0afcd8ece09da0444772ba52e5aef807d94ddba3
MrabEzreb <zerbebram@gmail.com>
2015-07-31 11:56:19 -0400

    :D
    Signed-off-by: MrabEzreb <zerbebram@gmail.com>

--- elements/consoleoutput.js
+++ elements/consoleoutput.js
@@ -1,13 +1,24 @@
-var console;
+var console, consoleTitle;
 function createConsole() {
     "use strict";
     var lconsole;
     lconsole = document.getElementsByTagName("consoleEl")[0];
-    if (lconsole != null) {
-        window.alert("happened");
+    if (lconsole !== null) {
+        //window.alert("happened");
         console = lconsole;
-    } else {
-        window.alert("happened2");
+        lconsole.style.overflowY = "auto";
+        consoleTitle = document.createElement("h1");
+        consoleTitle.innerHTML = lconsole.getAttribute("title");
+        consoleTitle.style.color = "white";
+        lconsole.appendChild(consoleTitle);
+        var oldHeight = lconsole.style.height;
+        lconsole.style.height = "calc("+oldHeight+" - 1em)";
+        lconsole.style.top = "1em";
+        consoleTitle.style.position = "fixed";
+        consoleTitle.style.marginTop = "-0.67em";
+        consoleTitle.style.backgroundColor = "black";
+        consoleTitle.style.width = "calc("+lconsole.style.width+" - 6px)";
+        //consoleInt = document.createElement("iframe");
     }
 }
 function writeToConsole(str) {
@@ -16,6 +27,20 @@ function writeToConsole(str) {
     elem = document.createElement("p");
     elem.innerHTML = str;
     elem.style.wordWrap = "normal";
+    elem.style.color = "white";
     console.appendChild(elem);
+    try {
+        console.scrollTop = console.scrollHeight;
+    } catch(e) {
+        var f = document.createElement("input");
+        f.setAttribute("type", "text");
+        console.appendChild(f);
+        f.focus();
+        console.removeChild(f);
+    }
+}
+function setConsoleTitle(str) {
+    "use strict";
+    consoleTitle.innerHTML = str;
 }
 window.addEventListener("load", createConsole);
--- loaders/loadblockly.js
+++ loaders/loadblockly.js
@@ -1,5 +1,6 @@
 var loader;
 loader = document.getElementsByTagName("head")[0];
+var blocklyLoaded = false;
 function addScript(name) {
     "use strict";
     var srcElem;
@@ -10,25 +11,35 @@ function addScript(name) {
 }
 function loadScripts() {
     "use strict";
-    addScript("blockly_compressed.js");
+    //addScript("blockly_compressed.js");
     addScript("blocks_compressed.js");
+    addScript("javascript_compressed.js");
     addScript("msg/js/en.js");
+    addScript("acorn_interpreter.js");
+    //addScript("generators/javascript/ezrebcode.js");
 }
+loadScripts();
+blocklyLoaded = true;
 function addBlocks() {
     var sysoutJson = {
       "id": "sysout",
-      "message0": "Text %1",
+      "message0": "Print To Console %1 Text %2",
       "args0": [
+        {
+          "type": "input_dummy",
+          "align": "CENTRE"
+        },
         {
           "type": "input_value",
           "name": "text",
-          "check": "String"
+          "check": "String",
+          "align": "RIGHT"
         }
       ],
       "inputsInline": false,
       "previousStatement": null,
       "nextStatement": null,
-      "colour": 120,
+      "colour": 160,
       "tooltip": "",
       "helpUrl": "http://www.example.com/"
     };
@@ -40,16 +51,76 @@ function addBlocks() {
     Blockly.JavaScript['sysout'] = function(block) {
       var value_text = Blockly.JavaScript.valueToCode(block, 'text', Blockly.JavaScript.ORDER_ATOMIC);
       // TODO: Assemble JavaScript into code variable.
-      var code = 'writeToConsole('+value_text+');';
+      var code = 'writeToConsole(\"'+value_text.slice(value_text.indexOf("(")+1, value_text.lastIndexOf(")"))+'\");';
       return code;
     };
+    var onRunJSON = {
+      "id": "runevent",
+      "message0": "Event %1 On Run %2",
+      "args0": [
+        {
+          "type": "input_dummy"
+        },
+        {
+          "type": "input_statement",
+          "name": "Code"
+        }
+      ],
+      "inputsInline": false,
+      "colour": 120,
+      "tooltip": "",
+      "helpUrl": "http://www.example.com/"
+    };
+    Blockly.Blocks['runevent'] = {
+      init: function() {
+        this.jsonInit(onRunJSON);
+      }
+    };
+    Blockly.JavaScript['runevent'] = function(block) {
+      var statements_code = Blockly.JavaScript.statementToCode(block, 'Code');
+      var code = 'function runEvent() {eval(\''+statements_code.replace(/'/g, "\\\'").replace(/"/g, "\\\"")+'\');}\nrunEvent();';
+      return code;
+    };
+    var stringJSON = {
+      "id": "string",
+      "message0": "\" %1 \"",
+      "args0": [
+        {
+          "type": "field_input",
+          "name": "NAME",
+          "text": ""
+        }
+      ],
+      "output": "String",
+      "colour": 160,
+      "tooltip": "",
+      "helpUrl": "http://www.example.com/"
+    };
+    Blockly.Blocks['string'] = {
+      init: function() {
+        this.jsonInit(stringJSON);
+      }
+    };
+    Blockly.JavaScript['string'] = function(block) {
+      var text_name = block.getFieldValue('NAME');
+      var code = text_name;
+      return [code, Blockly.JavaScript.ORDER_NONE];
+    };
 }
-loadScripts();
+function runCode() {
+    var code = Blockly.JavaScript.workspaceToCode(window.workspace);
+    try {
+        eval(code);
+    } catch (e) {
+        alert(e);
+    }
+}
 window.addEventListener("load", function() {
-    addBlocks();
     var blocklyArea = document.getElementById('blocklyArea');
     var blocklyDiv = document.getElementById('blocklyDiv');
+    addBlocks();
     var workspace = Blockly.inject(blocklyDiv, {toolbox: document.getElementById('toolbox')});
+    window.workspace = workspace;
     var onresize = function(e) {
     // Compute the absolute coordinates and dimensions of blocklyArea.
         var element = blocklyArea;
